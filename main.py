53f49061f6fed53f610ad901176646054e7587e3
Anderson Gomes <150376296+Gomezdev5@users.noreply.github.com>
2024-07-18 19:10:00 +0000

    V1

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 def q1():
+    '''Periodicamente as crianças brasileiras devem tomar vacinas que as protegem de diversas doenças. Escrever um programa para ler o ano em que a criança 
+    toma a 1a dose e a periodicidade (intervalo em anos) da vacina e exibir em que outros anos a criança deve tomar as próximas doses desta vacina, sabendo
+    que são 4(quatro) doses ao total.'''
+      
     pass
+        
+
 
 def q2():
+    """Faça um programa que leia uma sequencia de números e indique se eles são primos ou não. Você deve parar ao ler o número -1."""
+
     pass
 
+
 def q3():
+    """Valquíria trabalha em uma padaria e foi roubada ontem. Seus clientes ficaram com pena e resolveram organizar uma vaquinha para comprar um novo celular para ela.
+    Escreva um programa que receba como entrada o valor doado por cada cliente, até que seja informado um valor negativo, e exiba o total arrecadado e o valor médio doado
+    por eles."""
     pass
 
 def q4():
-    pass
+    """A Locadora de Veículos Eudora lançou uma grande promoção esse mês: pagando apenas R$ 90 por diária, o cliente pode alugar um carro de passeio. Para cada diária, 
+    o cliente recebe uma cota de quilometragem de 100 Km. Cada quilômetro a mais custará uma taxa extra de R$ 12.Escreva um programa que receba como entrada a quantidade 
+    de dias e a quilometragem total rodada por um cliente dessa locadora e exiba o valor total a ser pago com duas casas decimais."""
+    
 
+   
 def q5():
     pass
 
 if __name__=="__main__":
     # teste sua questão manualmente aqui
+    pass
